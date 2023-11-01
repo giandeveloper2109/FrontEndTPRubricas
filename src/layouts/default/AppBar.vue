f322b37f98cc9bb8f3d73c7d6e0dccfde093b265
Giancarlo Alvarez Gallardo <148255498+giandeveloper2109@users.noreply.github.com>
2023-10-31 19:17:31 -0500

    Primera Subida

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar  color="transparent">
+    <v-icon icon="$vuetify"></v-icon>
+    <v-app-bar-title >
+
+      D&G-Rubrics
+    </v-app-bar-title>
+    <v-spacer></v-spacer>
+    
+  </v-app-bar>
+</template>
+
+<script setup>
+  //
+</script>
